9d3baa2a552b4f62b69bb778eadae9661077ee75
sigurd4 <sigurd-s@online.no>
2026-05-25 00:12:00 +0200

    Implement Borrow for ColorConfig

--- src/config/color_config.rs
+++ src/config/color_config.rs
@@ -1,57 +1,77 @@
+use core::borrow::Borrow;
+
 use ratatui_3d::Rgb;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
+
+use crate::config::Config;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct ColorConfig
 {
-    #[serde(serialize_with = "serialize_rgb", deserialize_with = "deserialize_rgb")]
+    #[serde(with = "rgb")]
     pub surface: Rgb,
-    #[serde(serialize_with = "serialize_rgb", deserialize_with = "deserialize_rgb")]
+    #[serde(with = "rgb")]
     pub wire: Rgb,
-    #[serde(serialize_with = "serialize_rgb", deserialize_with = "deserialize_rgb")]
+    #[serde(with = "rgb")]
     pub dyed: Rgb,
-    #[serde(serialize_with = "serialize_rgb", deserialize_with = "deserialize_rgb")]
+    #[serde(with = "rgb")]
     pub glare: Rgb,
-    #[serde(serialize_with = "serialize_rgb", deserialize_with = "deserialize_rgb")]
+    #[serde(with = "rgb")]
     pub sun: Rgb
 }
 
-fn rgb_to_string(value: &Rgb) -> String
+impl Borrow<ColorConfig> for Config
 {
-    let Rgb(r, g, b) = value;
-
-    format!("{r:02X}{g:02X}{b:02X}")
-}
-fn str_to_rgb(src: &str) -> Rgb
-{
-    let rgb = u32::from_str_radix(src, 16)
-        .expect(&format!("Unable to parse RBG hexadecimal color '{src}'."));
-
-    assert!(rgb <= 0xFFFFFF, "RGB color cannot have alpha-channel.");
-
-    Rgb(
-        (rgb >> 16) as u8,
-        (rgb >> 8) as u8,
-        rgb as u8
-    )
+    fn borrow(&self) -> &ColorConfig
+    {
+        &self.color
+    }
 }
 
-fn serialize_rgb<S>(value: &Rgb, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer
+mod rgb
 {
-    let hex = rgb_to_string(value);
+    use ratatui_3d::Rgb;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-    hex.serialize(serializer)
-}
-fn deserialize_rgb<'de, D>(deserializer: D) -> Result<Rgb, D::Error>
-where
-    D: Deserializer<'de>
-{
-    let src = String::deserialize(deserializer)?;
+    use self as rgb;
 
-    Ok(str_to_rgb(&src))
+    pub(super) fn to_string(value: &Rgb) -> String
+    {
+        let Rgb(r, g, b) = value;
+
+        format!("{r:02X}{g:02X}{b:02X}")
+    }
+    pub(super) fn from_str(src: &str) -> Rgb
+    {
+        let rgb = u32::from_str_radix(src, 16)
+            .expect(&format!("Unable to parse RBG hexadecimal color '{src}'."));
+
+        assert!(rgb <= 0xFFFFFF, "RGB color cannot have alpha-channel.");
+
+        Rgb(
+            (rgb >> 16) as u8,
+            (rgb >> 8) as u8,
+            rgb as u8
+        )
+    }
+
+    pub fn serialize<S>(value: &Rgb, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        let hex = rgb::to_string(value);
+
+        hex.serialize(serializer)
+    }
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Rgb, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        let src = String::deserialize(deserializer)?;
+
+        Ok(rgb::from_str(&src))
+    }
 }
 
 #[cfg(test)]
@@ -59,18 +79,18 @@ mod test
 {
     use ratatui_3d::Rgb;
 
-use crate::config::color_config::{rgb_to_string, str_to_rgb};
+    use crate::config::color_config::rgb;
 
     #[test]
     fn test_hex()
     {
         let rgb = Rgb(0, 10, 0);
 
-        let hex = rgb_to_string(&rgb);
+        let hex = rgb::to_string(&rgb);
 
         println!("{}", hex);
 
-        let rgb_decoded = str_to_rgb(&hex);
+        let rgb_decoded = rgb::from_str(&hex);
 
         assert_eq!(rgb, rgb_decoded)
     }
