47fa800a52ca1e30c6bc1201201a6abee5ef7c39
sigurd4 <sigurd-s@online.no>
2026-05-25 01:37:24 +0200

    Shove digit into its own struct

--- src/config/edge_config.rs
+++ src/config/edge_config.rs
@@ -1,3 +1,5 @@
+use core::ops::Deref;
+
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -6,26 +8,45 @@ pub struct EdgeConfig
 {
     pub name: String,
     pub pivot: String,
-    #[serde(serialize_with = "serialize_digit", deserialize_with = "deserialize_digit")]
-    pub digit: Box<[[i8; 2]]>
+    pub digit: Digit
 }
 
-fn serialize_digit<S>(value: &Box<[[i8; 2]]>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Digit(Box<[[i8; 2]]>);
+
+impl Deref for Digit
 {
-    value.iter()
-        .map(|[x, y]| format!("{x},{y}"))
-        .collect::<Vec<_>>()
-        .join(" ")
-        .serialize(serializer)
+    type Target = [[i8; 2]];
+
+    fn deref(&self) -> &Self::Target
+    {
+        &self.0
+    }
 }
-fn deserialize_digit<'de, D>(deserializer: D) -> Result<Box<[[i8; 2]]>, D::Error>
-where
-    D: Deserializer<'de>
+
+impl Serialize for Digit
 {
-    Ok(
-        String::deserialize(deserializer)?
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        let Self(curve) = self;
+
+        curve.iter()
+            .map(|[x, y]| format!("{x},{y}"))
+            .collect::<Vec<_>>()
+            .join(" ")
+            .serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Digit
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        let curve = String::deserialize(deserializer)?
             .split(" ")
             .map(|xy| xy.split(",")
                 .map(|e| i8::from_str_radix(e, 10).expect("Failed parsing 8-bit int coordinate of digit."))
@@ -33,6 +54,8 @@ where
                 .try_into()
                 .expect("Each point of the digit must contain exactly two coordinates.")
             ).collect::<Vec<_>>()
-            .into()
-    )
+            .into();
+
+        Ok(Self(curve))
+    }
 }
